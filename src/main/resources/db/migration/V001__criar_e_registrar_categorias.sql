cc0de87a20d89491bb195ea7e4a9e1ecb3279855
Luciano Kogut <ljkogut@hotmail.com>
2023-11-18 11:54:08 -0300

    refact: adicionado as categorias de receitas

--- src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V001__criar_e_registrar_categorias.sql
@@ -3,9 +3,12 @@ CREATE TABLE categoria (
     nome        VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO categoria (nome) VALUES ('Salários');
+INSERT INTO categoria (nome) VALUES ('Trabalho Extra');
 INSERT INTO categoria (nome) VALUES ('Lazer');
 INSERT INTO categoria (nome) VALUES ('Alimentação');
 INSERT INTO categoria (nome) VALUES ('Supermercado');
 INSERT INTO categoria (nome) VALUES ('Farmácia');
 INSERT INTO categoria (nome) VALUES ('Mobilidade');
+INSERT INTO categoria (nome) VALUES ('Doação');
 INSERT INTO categoria (nome) VALUES ('Outros');
