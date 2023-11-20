2d6204a90274b0c70a3ecf3ca58242f616c107d5
Luciano Kogut <ljkogut@hotmail.com>
2023-11-20 00:47:11 -0300

    feat: implementado método para inclusão da categoria retornando parâmetro de Location e Body com categoriaSalva

--- src/main/java/br/com/lucianokogut/money/api/resource/CategoriaResource.java
+++ src/main/java/br/com/lucianokogut/money/api/resource/CategoriaResource.java
@@ -1,24 +1,41 @@
 package br.com.lucianokogut.money.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.lucianokogut.money.api.model.Categoria;
 import br.com.lucianokogut.money.api.repository.CategoriaRepository;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
-    
+
     @Autowired
     private CategoriaRepository categoriaRepository;
 
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
     }
 }
