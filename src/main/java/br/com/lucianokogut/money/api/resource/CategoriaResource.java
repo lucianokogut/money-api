c4a0c7adffa31abe0bb8f5e46b4778f1aad30343
Luciano Kogut <ljkogut@hotmail.com>
2023-11-19 23:57:29 -0300

    feat: implementado a classe de controle que fará toda intermediação entre a entidade, o objeto e a sua interface

---
+++ src/main/java/br/com/lucianokogut/money/api/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package br.com.lucianokogut.money.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lucianokogut.money.api.model.Categoria;
+import br.com.lucianokogut.money.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+    
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return categoriaRepository.findAll();
+    }
+}
