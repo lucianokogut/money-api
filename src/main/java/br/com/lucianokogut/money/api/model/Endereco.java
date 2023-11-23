a5783a9a84bee135fc22b7f5746c5e77f9975496
Luciano Kogut <ljkogut@hotmail.com>
2023-11-23 01:22:12 -0300

    feat: implementado a classe de endereço que será vinculada a classe pessoa de forma embutida e com todos os atributos necessários para controle deste CRUD futuramente pela API do ViaCEP

---
+++ src/main/java/br/com/lucianokogut/money/api/model/Endereco.java
@@ -0,0 +1,87 @@
+package br.com.lucianokogut.money.api.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+    private String localidade;
+    private String uf;
+    private String ddd;
+    private String ibge;
+    private String siafi;
+    private String gia;
+
+    // Região para os Getters e Setters
+    public String getLogradouro() {
+        return logradouro;
+    }
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public String getLocalidade() {
+        return localidade;
+    }
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+    public String getUf() {
+        return uf;
+    }
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    public String getDdd() {
+        return ddd;
+    }
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+    public String getIbge() {
+        return ibge;
+    }
+    public void setIbge(String ibge) {
+        this.ibge = ibge;
+    }
+    public String getSiafi() {
+        return siafi;
+    }
+    public void setSiafi(String siafi) {
+        this.siafi = siafi;
+    }
+    public String getGia() {
+        return gia;
+    }
+    public void setGia(String gia) {
+        this.gia = gia;
+    }
+
+}
