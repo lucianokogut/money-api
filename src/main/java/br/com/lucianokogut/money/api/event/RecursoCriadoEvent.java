09093c4375acbabccdc60c26a0b46a576b8b4090
Luciano Kogut <ljkogut@hotmail.com>
2023-11-22 23:33:05 -0300

    feat: implementado a classe que servirá de integração entre a troca de mensagens via URI para compor as respostas como JSON

---
+++ src/main/java/br/com/lucianokogut/money/api/event/RecursoCriadoEvent.java
@@ -0,0 +1,29 @@
+package br.com.lucianokogut.money.api.event;
+
+import org.springframework.context.ApplicationEvent;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+    private static final long serialVersionUID = 1L;
+
+    private HttpServletResponse response;
+    private Long codigo;
+
+    public RecursoCriadoEvent(Object source, HttpServletResponse response, Long codigo) {
+        super(source);
+        this.response = response;
+        this.codigo = codigo;
+    }
+
+    // Região para os Getters e Setters
+    public HttpServletResponse getResponse() {
+        return response;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+}
