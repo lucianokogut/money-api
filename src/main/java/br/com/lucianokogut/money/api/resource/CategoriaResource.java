53d73093856301ecf1f6ca486227e9c4d377a406
Luciano Kogut <ljkogut@hotmail.com>
2023-11-22 23:52:48 -0300

    refact: ajustado as bibliotecas que estão em uso, criado a função de publicação de eventos e removido o código da resposta via URI que foi desviado para Listener

--- src/main/java/br/com/lucianokogut/money/api/resource/CategoriaResource.java
+++ src/main/java/br/com/lucianokogut/money/api/resource/CategoriaResource.java
@@ -1,10 +1,11 @@
 package br.com.lucianokogut.money.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-
+import br.com.lucianokogut.money.api.event.RecursoCriadoEvent;
 import br.com.lucianokogut.money.api.model.Categoria;
 import br.com.lucianokogut.money.api.repository.CategoriaRepository;
 
@@ -31,15 +31,14 @@ public class CategoriaResource {
         return categoriaRepository.findAll();
     }
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @PostMapping
     public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
         Categoria categoriaSalva = categoriaRepository.save(categoria);
-
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-
-        return ResponseEntity.created(uri).body(categoriaSalva);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
     }
 
     @GetMapping("/{codigo}")
@@ -49,4 +48,5 @@ public class CategoriaResource {
         Optional<Categoria> categoria = categoriaRepository.findById(codigo);
 
         return categoria.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-    }}
+    }
+}
