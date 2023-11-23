e3a46594b70a89f23f90777e77011b1f5e771f4b
Luciano Kogut <ljkogut@hotmail.com>
2023-11-23 01:17:43 -0300

    feat: implementado a classe pessoa com os atributos necessários e obrigatórios para controle do CRUD de pessoas

---
+++ src/main/java/br/com/lucianokogut/money/api/model/Pessoa.java
@@ -0,0 +1,54 @@
+package br.com.lucianokogut.money.api.model;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    
+    @NotNull
+    private String nome;
+
+    @Embedded
+    private Endereco endereco;
+
+    @NotNull
+    private Boolean ativo;
+
+    // Região para os Getters e Setters
+    public Long getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    public Boolean getAtivo() {
+        return ativo;
+    }
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+}
