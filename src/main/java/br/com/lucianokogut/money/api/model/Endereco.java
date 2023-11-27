ff8a08d20ee92b766b2cfea05383bc39c53c2575
Luciano Kogut <ljkogut@hotmail.com>
2023-11-27 15:15:25 -0300

    refact: ajustado o alinhamento e formatação do código

--- src/main/java/br/com/lucianokogut/money/api/model/Endereco.java
+++ src/main/java/br/com/lucianokogut/money/api/model/Endereco.java
@@ -21,72 +21,95 @@ public class Endereco {
     public String getLogradouro() {
         return logradouro;
     }
+
     public void setLogradouro(String logradouro) {
         this.logradouro = logradouro;
     }
+
     public String getNumero() {
         return numero;
     }
+
     public void setNumero(String numero) {
         this.numero = numero;
     }
+
     public String getComplemento() {
         return complemento;
     }
+
     public void setComplemento(String complemento) {
         this.complemento = complemento;
     }
+
     public String getBairro() {
         return bairro;
     }
+
     public void setBairro(String bairro) {
         this.bairro = bairro;
     }
+
     public String getCep() {
         return cep;
     }
+
     public void setCep(String cep) {
         this.cep = cep;
     }
+
     public String getUnidade() {
         return unidade;
     }
+
     public void setUnidade(String unidade) {
         this.unidade = unidade;
     }
+
     public String getLocalidade() {
         return localidade;
     }
+
     public void setLocalidade(String localidade) {
         this.localidade = localidade;
     }
+
     public String getUf() {
         return uf;
     }
+
     public void setUf(String uf) {
         this.uf = uf;
     }
+
     public Integer getDdd() {
         return ddd;
     }
+
     public void setDdd(Integer ddd) {
         this.ddd = ddd;
     }
+
     public Integer getIbge() {
         return ibge;
     }
+
     public void setIbge(Integer ibge) {
         this.ibge = ibge;
     }
+
     public String getSiafi() {
         return siafi;
     }
+
     public void setSiafi(String siafi) {
         this.siafi = siafi;
     }
+
     public String getGia() {
         return gia;
     }
+
     public void setGia(String gia) {
         this.gia = gia;
     }
