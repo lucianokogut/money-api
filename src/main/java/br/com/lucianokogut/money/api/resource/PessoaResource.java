36f69463ba96a958c773661c89847e226ddee448
Luciano Kogut <ljkogut@hotmail.com>
2023-11-28 12:07:40 -0300

    refact: removido o método de findAll por este retornar uma Lista de Pessoas, que possui outros atributos embutidos

--- src/main/java/br/com/lucianokogut/money/api/resource/PessoaResource.java
+++ src/main/java/br/com/lucianokogut/money/api/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package br.com.lucianokogut.money.api.resource;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import br.com.lucianokogut.money.api.event.RecursoCriadoEvent;
 import br.com.lucianokogut.money.api.model.Pessoa;
 import br.com.lucianokogut.money.api.repository.PessoaRepository;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -26,10 +24,6 @@ public class PessoaResource {
 
     @Autowired
     private PessoaRepository pessoaRepository;
-
-    public List<Pessoa> listar() {
-        return pessoaRepository.findAll();
-    }
 
     @Autowired
     private ApplicationEventPublisher publisher;
