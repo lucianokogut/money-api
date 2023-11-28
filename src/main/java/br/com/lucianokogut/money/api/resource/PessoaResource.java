4ad456c00c8aabe86d5b6d03001acddc9e59b558
Luciano Kogut <ljkogut@hotmail.com>
2023-11-28 10:41:20 -0300

    feat: implementado a classe de controle de pessoas com os métodos básicos para criar, listar todos e buscar por código

---
+++ src/main/java/br/com/lucianokogut/money/api/resource/PessoaResource.java
@@ -0,0 +1,53 @@
+package br.com.lucianokogut.money.api.resource;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lucianokogut.money.api.event.RecursoCriadoEvent;
+import br.com.lucianokogut.money.api.model.Pessoa;
+import br.com.lucianokogut.money.api.repository.PessoaRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public List<Pessoa> listar() {
+        return pessoaRepository.findAll();
+    }
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+        // Ajustado o método para findById por causa do deprecated do findOne
+        // Corrigido para buscar pela chave primária do tipo Long
+        Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+
+        return pessoa.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+}
