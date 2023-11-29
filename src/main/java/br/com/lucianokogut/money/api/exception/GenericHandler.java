0b8ea37e7eb8ba8f8b144bc89e846001618d3475
Luciano Kogut <ljkogut@hotmail.com>
2023-11-29 20:36:32 -0300

    refact: implementado o Handler da exception para uso no pessoaOptional NULL

--- src/main/java/br/com/lucianokogut/money/api/exception/GenericHandler.java
+++ src/main/java/br/com/lucianokogut/money/api/exception/GenericHandler.java
@@ -3,6 +3,7 @@ package br.com.lucianokogut.money.api.exception;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -17,6 +18,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -45,6 +47,16 @@ public class GenericHandler extends ResponseEntityExceptionHandler {
 
         List<Erro> erros = criarListaErros(ex.getBindingResult());
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler({ NoSuchElementException.class })
+    public ResponseEntity<Object> handleNoSuchElementException (NoSuchElementException ex,
+            WebRequest request) {
+        String mensagemUsuario = messageSource.getMessage("recurso.nao.encontrado", null,
+                LocaleContextHolder.getLocale());
+        String mensagemDev = ex.toString();
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDev));
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     private List<Erro> criarListaErros(BindingResult bindingResult) {
