5947a16cb6c41ef31fe4340bfdef73770a84ad58
Luciano Kogut <ljkogut@hotmail.com>
2023-11-30 17:09:07 -0300

    feat: implementado método para exclusão de uma categoria informada pela código

--- src/main/java/br/com/lucianokogut/money/api/resource/CategoriaResource.java
+++ src/main/java/br/com/lucianokogut/money/api/resource/CategoriaResource.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -31,13 +33,13 @@ public class CategoriaResource {
     @Autowired
     private CategoriaService categoriaService;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
     }
-
-    @Autowired
-    private ApplicationEventPublisher publisher;
 
     @PostMapping
     public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
@@ -53,6 +55,12 @@ public class CategoriaResource {
         Optional<Categoria> categoria = categoriaRepository.findById(codigo);
 
         return categoria.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+        categoriaRepository.deleteById(codigo);
     }
 
     @PutMapping("/{codigo}")
