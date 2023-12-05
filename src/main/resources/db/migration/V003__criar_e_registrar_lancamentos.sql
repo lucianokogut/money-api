a01ea945bb5e604c3a97220c27d2d39a15994328
Luciano Kogut <ljkogut@hotmail.com>
2023-12-04 22:48:46 -0300

    feat: implementado a quantidade básica de registros para testar a criação da tabela e a inclusão dos exemplos iniciais

--- src/main/resources/db/migration/V003__criar_e_registrar_lancamentos.sql
+++ src/main/resources/db/migration/V003__criar_e_registrar_lancamentos.sql
@@ -13,8 +13,24 @@ CREATE TABLE lancamento (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-            VALUES ('Descrição 1', '2023-12-01', '2023-12-10', 100, 'Observação 1', 'DESPESA', 5, 1);
+            VALUES ('Salário mensal', '2023-12-05', '2023-12-05', 6720, 'Salário líquido', 'RECEITA', 1, 1);
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa)
-            VALUES ('Descrição 2', '2023-12-02', '2023-12-11', 100, 'Observação 2', 'DESPESA', 6, 2);
+            VALUES ('Trabalho de Pintura', '2023-12-10', '2023-12-10', 1500, 'Serviço de pintura na casa do vizinho', 'RECEITA', 2, 2);
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-            VALUES ('Descrição 3', '2023-12-03', '2023-12-12', 100, 'Observação 3', 'DESPESA', 7, 3);
+            VALUES ('Aluguel da Quadra de Futebol', '2023-12-05', '2023-12-10', 100, 'Divisão do valor mensal do aluguel da quadra', 'DESPESA', 1, 3);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
+            VALUES ('Aluguel da Quadra de Futebol', '2023-12-05', '2023-12-10', 100, 'Divisão do valor mensal do aluguel da quadra', 'DESPESA', 2, 3);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
+            VALUES ('Alimentação do Refeitório', '2023-12-10', '2023-12-15', 125, 'Valor dos Tickets cobrados pelo refeitório', 'DESPESA', 1, 4);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
+            VALUES ('Alimentação do Refeitório', '2023-12-10', '2023-12-15', 125, 'Valor dos Tickets cobrados pelo refeitório', 'DESPESA', 2, 4);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
+            VALUES ('Alimentação do Refeitório', '2023-12-10', '2023-12-15', 125, 'Valor dos Tickets cobrados pelo refeitório', 'DESPESA', 3, 4);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
+            VALUES ('Alimentação do Refeitório', '2023-12-10', '2023-12-15', 125, 'Valor dos Tickets cobrados pelo refeitório', 'DESPESA', 4, 4);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
+            VALUES ('Alimentação do Refeitório', '2023-12-10', '2023-12-15', 125, 'Valor dos Tickets cobrados pelo refeitório', 'DESPESA', 5, 4);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
+            VALUES ('Valor dos Vales Transportes', '2023-12-15', '2023-12-15', 150, 'Valor dos VTs do mês de Novembro/2023', 'DESPESA', 7, 5);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
+            VALUES ('Valor da Luz', '2023-12-03', '2023-12-15', 150, 'Valor da Luz relativo ao mês de Novembro/2023', 'DESPESA', 6, 9);
