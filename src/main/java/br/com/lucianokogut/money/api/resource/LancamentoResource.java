6d09c50738d8f7e4026a959eb17b18aedaaf0dfe
Luciano Kogut <ljkogut@hotmail.com>
2023-12-10 00:48:23 -0300

    feat: implementado métodos do recurso para: - criação dos novos lançamentos - atualização dos lançamentos por código - exclusão dos lançamentos por código feat: ajustado todas as bibliotecas necessárias

--- src/main/java/br/com/lucianokogut/money/api/resource/LancamentoResource.java
+++ src/main/java/br/com/lucianokogut/money/api/resource/LancamentoResource.java
@@ -5,14 +5,26 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.lucianokogut.money.api.event.RecursoCriadoEvent;
 import br.com.lucianokogut.money.api.model.Lancamento;
 import br.com.lucianokogut.money.api.repository.LancamentoRepository;
+import br.com.lucianokogut.money.api.service.LancamentoService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -21,9 +33,22 @@ public class LancamentoResource {
     @Autowired
     private LancamentoRepository lancamentoRepository;
 
+    @Autowired
+    private LancamentoService lancamentoService;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Lancamento> listar() {
         return lancamentoRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+        Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
     }
 
     @GetMapping("/{codigo}")
@@ -38,5 +63,17 @@ public class LancamentoResource {
 
         return lancamento.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
-    
+
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+        lancamentoRepository.deleteById(codigo);
+    }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Lancamento> atualizar(@PathVariable Long codigo, @Valid @RequestBody Lancamento lancamento) {
+        Lancamento lancamentoSalvo = lancamentoService.atualizar(codigo, lancamento);
+        return ResponseEntity.ok(lancamentoSalvo);
+    }
+
 }
